1651b82a58ccc70b1e959a818ce0a37c2794d136
Lucas Fernando Amorim <lf.amorim@yahoo.com.br>
2019-02-01 02:27:46 -0200

    test

--- test/test.php
+++ test/test.php
@@ -1,5 +1,7 @@
 <?php
 
+$full = file_get_contents("data.txt");
 $data = wordcompression_encode("dict.txt", file_get_contents("data.txt"));
 $compressed = wordcompression_decode("dict.txt", $data);
 printf("Compressed %f%%\n", (strlen($data) / strlen($compressed)) * 100);
+var_dump($full != $data);
